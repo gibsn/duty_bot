736cb41c4634351d2b656c0c1e54364c8e150334
Kirill Alekseev <gibsn@mail.ru>
2022-01-31 22:46:47 +0300

    Update internal/app/dutybot/dutybot.go

--- internal/app/dutybot/dutybot.go
+++ internal/app/dutybot/dutybot.go
@@ -85,7 +85,7 @@ func (bot *DutyBot) signalHandler() {
 	}
 }
 
-// Wait waits the bot finish gracefully
+// Wait waits for the bot finish gracefully
 func (bot *DutyBot) Wait() {
 	<-bot.finished
 }
